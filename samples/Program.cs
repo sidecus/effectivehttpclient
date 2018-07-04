7ed05c9e37f85124c01c69f9f04672ec8e7f4eaf
Zhengwen Zhu <zhezhu@microsoft.com>
2018-07-03 17:16:19 -0700

    update test program

--- samples/Program.cs
+++ samples/Program.cs
@@ -8,7 +8,7 @@
 
     class Program
     {
-        public static async Task MakeHttpGet()
+        public static async Task MakeHttpGet(Random random)
         {
             var baseAddress = new Uri(@"https://bing.com");
             var url = @"?toWww=1&redig=test";
@@ -21,33 +21,43 @@
             var renewStrategy = new RenewStrategy()
                 .UseAgeStrategy(new TimeSpan(1, 5, 0));
 
-            var random = new Random();
             using (var client = new EffectiveHttpClient(buildStrategy, renewStrategy))
             {
-                // Sleep random number of milliseconds (20 to 500) before hand to allow connection reuse
+                // Sleep random number of milliseconds (20 to 500) beforehand to allow connection reuse
                 // This simulates real time concurrency scenarios
-                Thread.Sleep(random.Next(20, 500));
+                Thread.Sleep(random.Next(100, 5000));
 
                 // Make the real call
+                var originatingThread = Thread.CurrentThread.ManagedThreadId;
+                Console.WriteLine($"{originatingThread}: started...");
+
                 var ret = await client.GetStringAsync(url).ConfigureAwait(false);
-                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} succeeded with {ret.Length} characters.");
-                Console.WriteLine(client.ToString());
+
+                var endingThread = Thread.CurrentThread.ManagedThreadId;
+                Console.WriteLine($"{originatingThread} -> {endingThread}: downloaded {ret.Length} characters. {client.ToString()}");
             }
         }
 
         static void Main(string[] args)
         {
             var tasks = new List<Task>();
-
+            var random = new Random();
+            
             // Make 10 concurrent calls against bing to simulate prod scenarios.
             // You should have less than 10 sockets againts www.bing.com.
             Console.WriteLine("Starting 10 threads with HTTP GET to www.bing.com with age strategy.");
             for (int i = 0; i < 10; i++)
             {
-                tasks.Add(MakeHttpGet());
+                var task = Task.Run(async () => {
+                    await MakeHttpGet(random);
+                });
+                
+                tasks.Add(task);
             }
             Task.WaitAll(tasks.ToArray());
-            Console.WriteLine("Please check socket number.");
+            Console.WriteLine("You can now check total connections to bing.com");
+
+            Thread.Sleep(60000);
         }
     }
 }
